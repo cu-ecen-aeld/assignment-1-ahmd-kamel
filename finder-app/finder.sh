cc4e3c4080cec43899ec94b89eefaf609301b5b1
Ahmed Sayed <a7med.skf@gmail.com>
2023-12-02 18:28:54 +0200

    Adding comment for more clearfication.

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -3,6 +3,7 @@
 filesdir=$1
 searchstr=$2
 
+# Check if the correct arguments is provided
 if [ -z "$filesdir" ]; then
     echo "Please put directory name."
     exit 1
@@ -11,7 +12,7 @@ elif [ -z "$searchstr" ]; then
     exit 1
 fi
 
-
+# Validate the directory that exist, count words and files
 if [ -d "$filesdir" ]; then
     files_count=$(find $filesdir -type f | wc -l)
     words_count=$(grep -r $searchstr $filesdir | wc -l)
